c226411a2e431c3a29a2483d962b8f3a990c4eff
CHIN <vominhtriet031105@gmail.com>
2025-03-14 14:00:07 +0700

    Postman Delete User

--- 01-java-spring-jobhunter-starter-master/src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
+++ 01-java-spring-jobhunter-starter-master/src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
@@ -5,7 +5,8 @@ import org.springframework.web.bind.annotation.RestController;
 import vn.hoidanit.jobhunter.domain.User;
 import vn.hoidanit.jobhunter.service.UserService;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -19,8 +20,8 @@ public class UserController {
         this.userService = userService;
     }
 
-    // @GetMapping("/user/create")
-    @PostMapping("/user/create")
+
+    @PostMapping("/user")
     public User createNewUser(
             @RequestBody User postmanUser) {
         
@@ -28,5 +29,10 @@ public class UserController {
         return user;
     }
 
+    @DeleteMapping("/user/{id}")
+    public String deleteUser(@PathVariable("id") long id) {
 
+        this.userService.handleDeleteUser(id);
+        return "Delete user";
+    }
 }
